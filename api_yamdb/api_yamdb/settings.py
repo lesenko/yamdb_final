ceffd8c11addb4b4f6f9f7c2f7026a561a0277d0
Olesya <lesenysh@yandex.ru>
2022-04-28 22:26:52 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,12 +7,12 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('TOP_SECRET')
+SECRET_KEY = os.getenv('TOP_SECRET', default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.98.48']
 
 # Application definition
 
